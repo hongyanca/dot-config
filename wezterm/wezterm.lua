882c94b5a98e1e699180319e99686c62ae4c07d6
Hong Yan <contact@hongyan.ca>
2024-03-13 01:09:49 -0600

    chore: update color scheme for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,8 +9,8 @@ local config = {
   font_size = 15.6,
   font = wezterm.font("DejaVuSansMono Nerd Font"),
   -- font = wezterm.font("BlexMono Nerd Font"),
-  color_scheme = 'Dissonance (Gogh)',
-  line_height = 1.08,
+  -- color_scheme = 'Dissonance (Gogh)',
+  line_height = 1.075,
 
   use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
@@ -29,10 +29,28 @@ local config = {
   }
 }
 
-config.inactive_pane_hsb = {
-  saturation = 0.7,
-  brightness = 0.7,
+
+local HomerColor = wezterm.color.get_builtin_schemes()['Dissonance (Gogh)']
+HomerColor.ansi[1] = '#000000'
+HomerColor.brights[1] = '#686a65'
+HomerColor.ansi[2] = '#fc4236'
+HomerColor.brights[2] = '#ff271a'
+HomerColor.ansi[3] = '#3d9970'
+HomerColor.brights[3] = '#2ecc41'
+HomerColor.ansi[4] = '#e3c701'
+HomerColor.brights[4] = '#f5be30'
+HomerColor.ansi[5] = '#3dbada'
+HomerColor.brights[5] = '#1588ff'
+HomerColor.ansi[6] = '#c634e1'
+HomerColor.brights[6] = '#e430b5'
+HomerColor.ansi[7] = '#3acbcc'
+HomerColor.brights[7] = '#217e7b'
+HomerColor.ansi[8] = '#c0c0c0'
+HomerColor.brights[8] = '#f1f1f1'
+config.color_schemes = {
+  ['Homer'] = HomerColor,
 }
+config.color_scheme = 'Homer'
 
 config.colors = {
   cursor_bg = '#7edbfd',
@@ -41,6 +59,12 @@ config.colors = {
   selection_fg = 'black',
   selection_bg = '#7edbfd',
 }
+
+config.inactive_pane_hsb = {
+  saturation = 0.7,
+  brightness = 0.7,
+}
+
 
 local mod = {}
 local platform = require('utils.platform')()
@@ -86,7 +110,7 @@ config.keys = {
     action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' })
   },
 }
--- This is where you actually apply your config choices
+
 
 -- and finally, return the configuration to wezterm
 return config
