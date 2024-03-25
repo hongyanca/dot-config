a26f4458a81f7a5a6c5e419c65f4125e940bfc43
Hong Yan <contact@hongyan.ca>
2024-03-25 01:46:17 -0600

    feat: add .zshrc

--- sync-config.sh
+++ sync-config.sh
@@ -4,3 +4,4 @@ rsync -avP --delete ~/.config/kitty ./
 rsync -avP --delete ~/.config/mpv ./
 rsync -avP --delete ~/.config/wezterm ./
 rsync -avP --delete ~/.config/starship ./
+rsync -avP --delete ~/.zshrc ./zshrc/_zshrc
