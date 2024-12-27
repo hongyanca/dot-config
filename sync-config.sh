7d80d134c0257cf1284e2867edfbde1354ccbcbd
Hong Yan <contact@yanhong.ca>
2024-12-26 17:13:28 -0700

    feat: add ghostty

--- sync-config.sh
+++ sync-config.sh
@@ -4,4 +4,5 @@ rsync -avP --delete ~/.config/kitty ./
 rsync -avP --delete ~/.config/mpv ./
 rsync -avP --delete ~/.config/wezterm ./
 rsync -avP --delete ~/.config/yazi ./
+rsync -avP --delete ~/.config/ghostty ./
 cat ~/.zshrc | grep -v 'API_KEY\|API KEY' >./zshrc/_zshrc
